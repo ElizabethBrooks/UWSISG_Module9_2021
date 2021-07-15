b544e8ef7804bde4a91d44a5e6899a7cc6499821
Elizabeth Brooks <merimaat1@gmail.com>
2021-07-15 14:14:32 -0400

    Add files via upload

--- Practical2.R
+++ Practical2.R
@@ -2,6 +2,7 @@
 setwd("C:/Users/brookse/Desktop/UWSISG_UW_2021")
 
 
+##HW ??2 tests
 #install.packages("devtools")
 #library(devtools)
 #install_github("jgx65/JGTeach")
@@ -26,3 +27,102 @@ plot(-log10(theo.pval),-log10(sort(p.val.x2)),
      ylab="emp p-val dist x2");abline(c(0,1))
 
 
+
+x<-0:1000/1000
+maf01<-which(pan@snps$maf>=0.01)
+nl<-length(maf01)
+theo.pval<-1:nl/nl
+plot(-log10(theo.pval),-log10(sort(p.val.x2[maf01])),
+     col="red",cex=0.5,xlab="Theo p val dist",
+     ylab="emp p-val dist x2");abline(c(0,1))
+plot(pan[,maf01]@p,pan[,maf01]@snps$hz,col="black",pch=16,cex=0.6)
+lines(x,2*x*(1-x),col="blue")
+outliers<-which(-log10(p.val.x2[maf01])>4)
+points(pan[,maf01][,outliers]@p,pan[,maf01][,outliers]@snps$hz,col="red",pch=16,cex=0.6)
+
+
+par(mfrow=c(1,2))
+#what frequency leads to np^2==1 e.g. p=(1/n)^0.5
+#nb inds
+ni<-dim(pan)[1]
+xi<-1
+mafn1<-which(pan@snps$maf>=(xi/ni)^.5)
+nl<-length(mafn1)
+theo.pval<-1:nl/nl
+plot(-log10(theo.pval),-log10(sort(p.val.x2[mafn1])),
+     col="red",cex=0.5,xlab="Theo p val dist",
+     ylab="emp p-val dist x2",main=expression(np^2>=1));abline(c(0,1))
+
+#what frequency leads to np^2==5
+xi<-5
+mafn5<-which(pan@snps$maf>=(xi/ni)^.5)
+nl<-length(mafn5)
+theo.pval<-1:nl/nl
+plot(-log10(theo.pval),-log10(sort(p.val.x2[mafn5])),
+     col="red",cex=0.5,xlab="Theo p val dist",
+     ylab="emp p-val dist x2",main=expression(np^2>=5));abline(c(0,1))
+par(mfrow=c(1,1))
+
+
+##HW exact tests
+install.packages("broom")
+install.packages("mice")
+install.packages("HardyWeinberg")
+library(broom)
+library(mice)
+library(HardyWeinberg)
+
+
+hw.ex<-HWExactStats(cbind(pan@snps$N0,pan@snps$N1,pan@snps$N2),midp=FALSE)
+hw.mp<-HWExactStats(cbind(pan@snps$N0,pan@snps$N1,pan@snps$N2),midp=TRUE)
+nl<-length(hw.mp)
+par(mfrow=c(1,2))
+plot(-log10(1:nl/nl),-log10(sort(hw.ex)),cex=0.6,pch=16);abline(c(0,1))
+plot(-log10(1:nl/nl),-log10(sort(hw.mp)),cex=0.6,pch=16);abline(c(0,1))
+par(mfrow=c(1,1))
+
+
+ch22<-read.VCF("chr22_Mb0_20.recode.vcf.gz")
+samp.desc.file<-"https://www2.unil.ch/popgen/teaching/SISG18/integrated_call_samples_v3.20130502.ALL.panel"
+samp.desc<-read.table(samp.desc.file,header=TRUE)
+EAS<-which(samp.desc$super_pop=="EAS")
+plot(ch22[EAS,]@p,ch22[EAS,]@snps$hz,col="red",pch=16,cex=0.6)
+lines(x,2*x*(1-x),col="blue")
+
+
+hw.mp.EAS<-HWExactStats(cbind(ch22[EAS,]@snps$N0,ch22[EAS,]@snps$N1,ch22[EAS,]@snps$N2),midp=TRUE)
+
+
+nl<-length(hw.mp.EAS)
+plot(-log10(1:nl/nl),-log10(sort(hw.mp.EAS)),cex=0.6,pch=16);abline(c(0,1))
+outliers<-which(-log10(hw.mp.EAS)>6)
+plot(ch22[EAS,]@p,ch22[EAS,]@snps$hz,col="black",pch=16,cex=0.6)
+lines(x,2*x*(1-x),col="blue")
+points(ch22[EAS,outliers]@p,ch22[EAS,outliers]@snps$hz,col="red",pch=16,cex=0.6)
+
+
+##Power to detect HW departure [optional]
+ni<-100
+f<-0.125
+pchisq(qchisq(0.95,df=1),df=1,ncp=ni*f^2,lower=FALSE)
+
+#density of chisq with ncp nf2
+
+x<-seq(0.2,20,0.1)
+plot(x,dchisq(x,df=1),type="h",col="#FF000080",
+     xlab=expression(chi^2),ylab="probability density") #chisq prob dens
+lines(x,dchisq(x,df=1,ncp=ni*f^2),type="h",col="#0000FF80") 
+abline(v=qchisq(0.95,df=1)) # 95th centile of chisq dist 
+
+
+ns<-1:10*100 
+round(pchisq(qchisq(0.95,df=1),df=1,ncp=ns*f^2,lower=FALSE),digits=3)
+
+ni<-500
+f<-0.125
+pchisq(qchisq(0.95,df=1),df=1,ncp=ni*f^2,lower=FALSE)
+plot(x,dchisq(x,df=1),type="h",col="#FF000080",
+     xlab=expression(chi^2),ylab="probability density")
+lines(x,dchisq(x,df=1,ncp=ni*f^2),type="h",col="#0000FF80") 
+#density of chisq with ncp nf2
+abline(v=qchisq(0.95,df=1)) # 95th centile of chisq dist 
